8a6be2038607da36b4f7eef1aa509b8889f7c7b7
Franco Gallardini <lucianogallardini64@gmail.com>
2022-06-06 16:11:15 -0300

    card borded added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,9 @@
+import './Card.css'
 
 function Card(props) {
     return(
         <div className="container">
+            <h1>{props.header}</h1>
             <p>{props.title}</p>
             {props.children}
         </div>
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Card from '../../components/Card/Card';
 import Input from '../../components/Input/Input';
+import './Login.css'
 
 function Login(){
     return(
-        <Card title="Login Card">
+        <Card title="Login Card" >
             <Input placeholder="username"/>
             <Input placeholder="password"/>
         </Card>
